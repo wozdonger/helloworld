718fadfa84398287b67bdb4e67e06b636f6a1c29
wozdonger <wozdonger@gmail.com>
2021-01-10 15:32:05 +0700

    Deploy my helloworld react-app to github

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="bg"></div>
+      <div className="Hello">
+        Hello World!
+        <br/>
+        I'm Wozdonger
+      </div>
+    </div>
+  );
+}
+
+export default App;
